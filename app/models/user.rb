adf40feaaa8728ece1dbb79b90c9442398a08157
Jonathan Yeong <hey@jonathanyeong.com>
2025-01-14 12:44:38 +0000

    User model can now create anonymous users
    Skips the password validation for anonymous users.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,20 @@
+require "securerandom"
+
 class User < ApplicationRecord
   has_secure_password
   has_many :sessions, dependent: :destroy
-
   normalizes :email_address, with: ->(e) { e.strip.downcase }
+
+  enum :provider_type, anonymous: "anonymous"
+
+  def errors
+    super.tap { |errors| errors.delete(:password, :blank) if self.anonymous? }
+  end
+
+  def self.create_anonymous!
+    create!(
+      uid: SecureRandom.uuid,
+      provider_type: "anonymous"
+    )
+  end
 end
